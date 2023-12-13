8d5e1f38ab4d0ef4e6af8c4a4cc1aca1c974a656
vero <veronica.delotero@gmail.com>
2023-12-12 22:39:08 -0300

    Descripción de los cambios en vero-rama

--- js/script.js
+++ js/script.js
@@ -1,35 +1,61 @@
 function validarFormulario() {
-    var nombre = document.getElementById("nombre").value;
-    var apellido = document.getElementById("apellido").value;
-    var edad = document.getElementById("edad").value;
-    var email = document.getElementById("email").value;
-    var celular = document.getElementById("celular").value;
-    var consulta = document.getElementById("consulta").value;
-    var errorMessage = document.getElementById("error-message");
+    // Obtener los valores ingresados por el usuario y recortar
+    // los posibles espacios en blanco al principio y al final.
+    var nombre = document.getElementById("nombre").value.trim();
+    var apellido = document.getElementById("apellido").value.trim();
+    var celular = document.getElementById("celular").value.trim();
+    var consulta = document.getElementById("consulta").value.trim();
+    var email = document.getElementById("email").value.trim();  // Agrega esta línea
 
-    if (!camposCompletos(nombre, apellido, edad, email, celular, consulta)) {
-        errorMessage.textContent = "Todos los campos son obligatorios. Por favor, complételos.";
+    // Verificar si algún campo está en blanco
+    if (nombre === "" || apellido === "" || celular === "" || email === ""|| consulta === "" ) {
+        alert("Por favor, complete todos los campos del formulario.");
         return false;
     }
 
-    if (!validarEmail(email)) {
-        errorMessage.textContent = "El campo de correo electrónico debe contener un '@'.";
-        return false;
-    }
-
-    errorMessage.textContent = "";
-    return true;
-}
-
-function camposCompletos() {
-    for (var i = 0; i < arguments.length; i++) {
-        if (!arguments[i]) {
+    // Verificar si el nombre contiene solo caracteres alfabéticos y espacios
+    for (var i = 0; i < nombre.length; i++) {
+        var charCode = nombre.charCodeAt(i);
+        if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32)) {
+            alert("El campo 'nombre' solo puede contener caracteres alfabéticos y espacios.");
             return false;
         }
     }
-    return true;
-}
 
-function validarEmail(email) {
-    return email.includes("@");
-}
+    // Verificar si el apellido contiene solo caracteres alfabéticos y espacios
+    for (var i = 0; i < apellido.length; i++) {
+        var charCode = apellido.charCodeAt(i);
+        if (!((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122) || charCode === 32)) {
+            alert("El campo 'apellido' solo puede contener caracteres alfabéticos y espacios.");
+            return false;
+        }
+    }
+
+    // Verificar si el celular contiene solo 8 dígitos numéricos
+
+        if (celular.length !== 11) {
+            alert("El campo 'celular' debe contener exactamente 11 dígitos numéricos.");
+            return false;
+        }
+        for (var j = 0; j < celular.length; j++) {
+            var digit = celular.charAt(j);
+            if (digit < "0" || digit > "9") {
+                alert("El campo 'celular' solo puede contener dígitos numéricos.");
+                return false;
+            }
+        }
+    
+        // Validar formato del correo electrónico
+        if (!validarEmail(email)) {
+            alert("El campo de correo electrónico debe ser válido.");
+            return false;
+        }
+    
+        // Si todas las validaciones son exitosas, enviar el formulario
+        alert("Formulario enviado correctamente.");
+        return true;
+    }
+    
+    function validarEmail(email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
